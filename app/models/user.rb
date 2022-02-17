c17f37913ee24ca8e18305323e7771d01c67f4f8
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-02-17 11:53:41 +0100

    Add Comment model and reference to post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
+
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 5}
 end
